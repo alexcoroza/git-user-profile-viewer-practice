5d88c71bf15ab2360eff508489c596f693b3809b
Alex Coroza <coroza.alex.urriza@gmail.com>
2020-01-23 00:24:21 +0800

    changed method getUsers into searchUsers and now accepts username as search parameter

--- src/app/services/user.service.spec.ts
+++ src/app/services/user.service.spec.ts
@@ -1,8 +1,9 @@
 import { TestBed } from '@angular/core/testing';
 import { HttpClientModule, HttpClient } from '@angular/common/http';
-import { of } from 'rxjs';
+import { of, EMPTY } from 'rxjs';
+import { catchError } from 'rxjs/operators';
 
-import { getUsersMockData }  from '../mock-data/get-users';
+import { searchUsersMockData }  from '../mock-data/search-users';
 import { UserService } from './user.service';
 
 describe('UserService', () => {
@@ -26,19 +27,31 @@ describe('UserService', () => {
 	});
 
 
-	describe('getUsers', () => {
+	describe('searchUsers', () => {
 		let httpGetSpy;
 
 		beforeEach(() => {
-			httpGetSpy = spyOn(TestBed.get(HttpClient), 'get').and.returnValue(of(getUsersMockData));
+			httpGetSpy = spyOn(TestBed.get(HttpClient), 'get').and.returnValue(of(searchUsersMockData));
 		});
 
 		it('should return User[] if theres no error', (done) => {
-			service.getUsers()
+			service.searchUsers(null)
 			.subscribe((users) => {
 				expect(users).toBeDefined();
 				done();
 			});
+		});
+
+		it('should return an Error if theres an error in parsing the result', (done) => {
+			httpGetSpy.and.returnValue(of('some unknown data that cannot be parsed'));
+			service.searchUsers('alex')
+			.pipe(
+				catchError((error) => {
+					expect(error.message).toEqual('Data parsing error in searchUsers');
+					done();
+					return EMPTY;
+				})
+			).subscribe();
 		});
 	});
 	
--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { Observable, of, throwError } from 'rxjs';
+import { switchMap } from 'rxjs/operators';
 
 import { environment } from '../../environments/environment';
 import { User } from '../interfaces/user';
@@ -16,9 +17,72 @@ export class UserService {
 	) { }
 
 
-	getUsers() : Observable<User> {
-		const url = `${environment.apiUrl}/users`;
-		return this.httpClient.get<User>(url);
+	/**
+	 * Get users from github.
+	 * May return HttpErrorResponse or Error.
+	 */
+	searchUsers(username: string) : Observable<User[]> {
+		const url = `${environment.apiUrl}/search/users?q=${username}`;
+		return this.httpClient.get(url)
+		.pipe(
+			switchMap((result) => {
+				try {
+					// map the api result into our own object
+					return of(this.mapResultToUsers(result));
+				} catch(error) {
+					const newError = new Error('Data parsing error in searchUsers');
+					return throwError(newError);
+				}
+			})
+		);
+	}
+
+
+
+	/**
+	 * Map results into our own User object
+	 * @param result Result of searching users from github api
+	 */
+	private mapResultToUsers(result): User[] {
+		const mappedUsers = [];
+		
+		result.items.forEach((user) => {
+			mappedUsers.push({
+				login: user.login,
+				id: user.id,
+				nodeId: user.node_id,
+				avatarUrl: user.avatar_url,
+				gravatarId: user.gravatar_id,
+				url: user.url,
+				htmlUrl: user.html_url,
+				followersUrl: user.followers_url,
+				followingUrl: user.following_url,
+				gistsUrl: user.gists_url,
+				starredUrl: user.starred_url,
+				subscriptionsUrl: user.subscriptions_url,
+				organizationsUrl: user.organizations_url,
+				reposUrl: user.repos_url,
+				eventsUrl: user.events_url,
+				receivedEventsUrl: user.received_events_url,
+				type: user.type,
+				siteAdmin: user.site_admin,
+				name: user.name,
+				company: user.company,
+				blog: user.blog,
+				location: user.location,
+				email: user.email,
+				hireable: user.hireable,
+				bio: user.bio,
+				publicRepos: user.public_repos,
+				publicGists: user.public_gists,
+				followers: user.followers,
+				following: user.following,
+				createdAt: new Date(user.created_at),
+				updatedAt: new Date(user.updated_at)
+			});
+		});
+
+		return mappedUsers;
 	}
 	
 }
