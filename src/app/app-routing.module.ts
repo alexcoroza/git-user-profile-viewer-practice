30b5bdc3f5e8ef5f5d41c676d1f10659b0b032e4
Alex Coroza <coroza.alex.urriza@gmail.com>
2020-01-23 09:34:52 +0800

    added design to user-profiles-page component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { UserProfilesPageComponent } from './components/pages/user-profiles-page/user-profiles-page.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+	{ path: '', redirectTo: '/user-profiles', pathMatch: 'full' },
+	{ path: 'user-profiles', component: UserProfilesPageComponent }
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+	imports: [RouterModule.forRoot(routes)],
+	exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -25,11 +25,4 @@ describe('AppComponent', () => {
     const app = fixture.debugElement.componentInstance;
     expect(app.title).toEqual('git-user-profile-viewer');
   });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('git-user-profile-viewer app is running!');
-  });
 });
